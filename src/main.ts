f160c218cea0707fbd02510a6608caaa12c758b0
CesarBaquiro <cmbm.kf@gmail.com>
2024-10-21 14:17:49 -0500

    Update package.json

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,4 @@ platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((err) => console.error(err));
 
-//-----------------------Code for deploy-------------------------
-// ng deploy --base-href=https://CesarBaquiro.github.io/chips/
+
